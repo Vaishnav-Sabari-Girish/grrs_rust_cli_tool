2c2e93ac99d5c2f5ba5f660ebfa58652757253ba
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-26 17:46:22 +0530

    Added clap for parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No Patterns given");
-    let path = std::env::args().nth(2).expect("No Path given");
 
-    let arguments = Cli {
-        pattern, 
-        path: std::path::PathBuf::from(path),
-    };
-
+    let arguments = Cli::parse();
     print!("pattern: {:?}, path: {:?}", arguments.pattern, arguments.path);
 }
