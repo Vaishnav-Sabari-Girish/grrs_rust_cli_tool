a8efb2a3226da627fde57fc140afa8de96edc7dd
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-26 17:42:57 +0530

    Added struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     let pattern = std::env::args().nth(1).expect("No Patterns given");
     let path = std::env::args().nth(2).expect("No Path given");
 
-    print!("pattern: {:?}, path: {:?}", pattern, path);
+    let arguments = Cli {
+        pattern, 
+        path: std::path::PathBuf::from(path),
+    };
+
+    print!("pattern: {:?}, path: {:?}", arguments.pattern, arguments.path);
 }
