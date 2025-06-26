3c6208de2f72f67b2d5ece4ebebe745260e66155
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-26 17:34:16 +0530

    Added pattern, path checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("No Patterns given");
+    let path = std::env::args().nth(2).expect("No Path given");
+
+    print!("pattern: {:?}, path: {:?}", pattern, path);
 }
